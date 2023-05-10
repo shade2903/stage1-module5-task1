1c5077c1e04b5370150acc2b14b83400bd9046d7
Vladimir Haiduk <nightshade2903@gmail.com>
2023-05-10 20:27:18 +0300

    completed

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -2,6 +2,7 @@ package com.epam.mjc;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -27,9 +28,9 @@ public class InterfaceCreator {
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
         return x -> {
             List<Integer> list = new ArrayList<>();
-            list.addAll(list);
-            for(Integer e : list){
-                if(e % 2 == 0){
+            list.addAll(x);
+            for (Integer e : list) {
+                if (e % 2 == 0) {
                     x.add(e);
                 }
             }
@@ -37,23 +38,34 @@ public class InterfaceCreator {
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-       return () -> {
-           List<String> copyValues = new ArrayList<>();
-            for(String str : values){
-                if(str.substring(0,1).equals(str.substring(0,1).toUpperCase())
-                && str.charAt(str.length() -1) == ('.')
-                && str.trim().split(" ").length >= 3);
-                copyValues.add(str);
+        return () -> {
+            List<String> copyValues = new ArrayList<>();
+            for (String str : values) {
+                if (str.trim().substring(0, 1).equals(str.trim().substring(0, 1).toUpperCase())
+                        && str.charAt(str.length() - 1) == ('.')
+                        && str.trim().split(" ").length > 3) {
+                    copyValues.add(str);
+                }
             }
+
             return copyValues;
-       };
+        };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x ->{
+            HashMap<String, Integer> map = new HashMap<>();
+            for(String str : x){
+                map.put(str,str.length());
+            }
+            return map;
+        };
     }
 
     public BiFunction<List<Integer>, List<Integer>, List<Integer>> concatList() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return (list1, list2) -> {
+            list1.addAll(list2);
+            return list1;
+        };
     }
 }
