5d0a9adf28c74ddb07e18dfdc108c5f974bce712
shade2903 <nighshade2903@gmail.com>
2023-05-10 16:51:52 +0300

    completer three methods

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,5 +1,7 @@
 package com.epam.mjc;
 
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.function.BiFunction;
@@ -11,15 +13,40 @@ import java.util.function.Supplier;
 public class InterfaceCreator {
 
     public Predicate<List<String>> isValuesStartWithUpperCase() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            for (String s : x) {
+                if (s.charAt(0) < 65 || s.charAt(0) > 90) {
+                    return false;
+                }
+            }
+            return true;
+        };
     }
 
+
     public Consumer<List<Integer>> addEvenValuesAtTheEnd() {
-        throw new UnsupportedOperationException("You should implement this method.");
+        return x -> {
+            List<Integer> list = new ArrayList<>();
+            list.addAll(list);
+            for(Integer e : list){
+                if(e % 2 == 0){
+                    x.add(e);
+                }
+            }
+        };
     }
 
     public Supplier<List<String>> filterCollection(List<String> values) {
-        throw new UnsupportedOperationException("You should implement this method.");
+       return () -> {
+           List<String> copyValues = new ArrayList<>();
+            for(String str : values){
+                if(str.substring(0,1).equals(str.substring(0,1).toUpperCase())
+                && str.charAt(str.length() -1) == ('.')
+                && str.trim().split(" ").length >= 3);
+                copyValues.add(str);
+            }
+            return copyValues;
+       };
     }
 
     public Function<List<String>, Map<String, Integer>> stringSize() {
